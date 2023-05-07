41ed2a4834fa8d78f304a27f3ee4ba79a22ad7cc
aden kesuma <adenfdfd10@gmail.com>
2023-05-07 20:32:31 +0700

    modified file constants and sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,56 +1,7 @@
-// icon import
-import HomeIcon from '@mui/icons-material/Home'
-import CodeIcon from '@mui/icons-material/Code'
-import OndemandVideoIcon from '@mui/icons-material/OndemandVideo'
-import SportsEsportsIcon from '@mui/icons-material/SportsEsports'
-import LiveTvIcon from '@mui/icons-material/LiveTv'
-import SchoolIcon from '@mui/icons-material/School'
-import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural'
-import CheckroomIcon from '@mui/icons-material/Checkroom'
-import GraphicEqIcon from '@mui/icons-material/GraphicEq'
-import TheaterComedyIcon from '@mui/icons-material/TheaterComedy'
-import FitnessCenterIcon from '@mui/icons-material/FitnessCenter'
-import DeveloperModeIcon from '@mui/icons-material/DeveloperMode'
-import MusicNoteIcon from "@mui/icons-material/MusicNote";
-
-// package import
 import { Icon, Stack } from '@mui/material'
 import { categories } from '../utils/constants'
 
 export default function Sidebar({ selectedCategory, setSelectedCategory }) {
-    const getCategoryIcon = (iconName) => {
-        switch (iconName) {
-            case 'HomeIcon':
-                return <HomeIcon />
-            case 'CodeIcon':
-                return <CodeIcon />
-            case 'OndemandVideoIcon':
-                return <OndemandVideoIcon />
-            case 'SportsEsportsIcon':
-                return <SportsEsportsIcon />
-            case 'LiveTvIcon':
-                return <LiveTvIcon />
-            case 'SchoolIcon':
-                return <SchoolIcon />
-            case 'FaceRetouchingNaturalIcon':
-                return <FaceRetouchingNaturalIcon />
-            case 'CheckroomIcon':
-                return <CheckroomIcon />
-            case 'GraphicEqIcon':
-                return <GraphicEqIcon />
-            case 'TheaterComedyIcon':
-                return <TheaterComedyIcon />
-            case 'FitnessCenterIcon':
-                return <FitnessCenterIcon />
-            case 'DeveloperModeIcon':
-                return <DeveloperModeIcon />
-            case 'MusicNoteIcon':
-                return <MusicNoteIcon />
-            default:
-                return null;
-        }
-    }
-
     return (
         <Stack
             direction='row'
@@ -63,7 +14,7 @@ export default function Sidebar({ selectedCategory, setSelectedCategory }) {
                     className='category-btn'
                     style={{ background: category.name === selectedCategory && '#FC1503', color: '#fff'}}
                 >
-                    <Icon style={{ color: category.name === selectedCategory ? '#fff' : 'red' }}>{getCategoryIcon(category.icon)}</Icon>
+                    <Icon style={{ color: category.name === selectedCategory ? '#fff' : 'red' }}>{category.icon}</Icon>
                     <span>{category.name}</span>
                 </button>
             ))}
---
+++ src/utils/constants.jsx
@@ -0,0 +1,41 @@
+import HomeIcon from '@mui/icons-material/Home'
+import CodeIcon from '@mui/icons-material/Code'
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo'
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports'
+import LiveTvIcon from '@mui/icons-material/LiveTv'
+import SchoolIcon from '@mui/icons-material/School'
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural'
+import CheckroomIcon from '@mui/icons-material/Checkroom'
+import GraphicEqIcon from '@mui/icons-material/GraphicEq'
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy'
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter'
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode'
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png'
+
+export const categories = [
+    { name: 'New', icon: <HomeIcon /> },
+    { name: 'Josh tried coding', icon: <CodeIcon /> },
+    { name: 'Coding', icon: <CodeIcon /> },
+    { name: 'ReactJS', icon: <CodeIcon />  },
+    { name: 'NextJS', icon: <CodeIcon />  },
+    { name: 'Music', icon: <MusicNoteIcon /> },
+    { name: 'Education', icon: <SchoolIcon />  },
+    { name: 'Podcast', icon: <GraphicEqIcon /> },
+    { name: 'Movie', icon: <OndemandVideoIcon /> },
+    { name: 'Gaming', icon: <SportsEsportsIcon /> },
+    { name: 'Live', icon: <LiveTvIcon />  },
+    { name: 'Sport', icon: <FitnessCenterIcon /> },
+    { name: 'Fashion', icon: <CheckroomIcon /> },
+    { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />},
+    { name: 'Comedy', icon: <TheaterComedyIcon /> },
+    { name: 'Gym', icon: <FitnessCenterIcon /> },
+    { name: 'Crypto', icon: <DeveloperModeIcon /> },
+]
+
+export const demoThumnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png'
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A'
+export const demoVideoUrl = '/video/GDa8kZLNhJ4'
+export const demoChannelTitle = 'Javascript Mastery'
+export const demoVideoTitle = 'Build and Deploy 5 Javascript & React API Project in 10 hours - Full-course | RapidAPI'
