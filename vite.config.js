97203b2eb9d064c1e73b4ad994ac62087cb29dc3
aden kesuma <adenfdfd10@gmail.com>
2023-05-11 22:38:11 +0700

    change configure deploy

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Steam-scape/",
   build: {
     outDir: 'dist'
-  }
+  },
+  html: {
+    inject: {
+      injectData: {
+        type: 'module',
+      },
+    },
+  },
 })   
 
 
